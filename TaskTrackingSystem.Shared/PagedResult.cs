fdab762f78d921137e575409cac6561419a79ef3
User <User@LAPTOP-27EJT6A8>
2026-06-22 16:00:39 +0630

    add: pagination

---
+++ TaskTrackingSystem.Shared/PagedResult.cs
@@ -0,0 +1,10 @@
+namespace TaskTrackingSystem.Shared;
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; set; } = new();
+    public int TotalCount { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+    public int TotalPages { get; set; }
+}
---
+++ TaskTrackingSystem.Shared/PaginationQuery.cs
@@ -0,0 +1,8 @@
+namespace TaskTrackingSystem.Shared;
+
+public class PaginationQuery
+{
+    public int? Page { get; set; }
+
+    public int? Limit { get; set; }
+}
